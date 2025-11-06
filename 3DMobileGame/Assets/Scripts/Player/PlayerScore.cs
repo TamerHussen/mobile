3909309140e3c59dab1928ab1fb920fd1e12bf44
TamerHussen <tamer.hussen@mail.bcu.ac.uk>
2025-11-06 13:52:32 +0000

    added collectible limit
    add code in player score and updated unity scenes to show players the amount of collectables on the map.

--- 3DMobileGame/Assets/Scripts/Player/PlayerScore.cs
+++ 3DMobileGame/Assets/Scripts/Player/PlayerScore.cs
@@ -3,27 +3,46 @@ using TMPro;
 
 public class PlayerScore : MonoBehaviour
 {
-    public int score = 0;
-    public TextMeshProUGUI scoreText;
+    public int Score = 0;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI CollectedText;
+    public int Collected = 0;
+    public int MaxCollectibles;
 
     private void Start()
     {
+
+        GameObject[] collectibles = GameObject.FindGameObjectsWithTag("Collectible");
+        MaxCollectibles = collectibles.Length;
+
         UpdateScoreUI();
+        UpdateCollectedUI();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Collectible"))
         {
-            score += 10;
+            Score += 10;
+            Collected++;
+
             UpdateScoreUI();
+            UpdateCollectedUI();
+
             Destroy(other.gameObject);
         }
     }
 
     void UpdateScoreUI()
     {
-        if (scoreText != null)
-            scoreText.text = "Score: " + score;
+        if (ScoreText != null)
+            ScoreText.text = "Score: " + Score;
+    }
+
+    void UpdateCollectedUI()
+    {
+        if (CollectedText != null)
+            CollectedText.text = $"Collected: {Collected} / {MaxCollectibles}";
     }
 }
